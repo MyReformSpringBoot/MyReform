1fbf591367eabec51d8cbd1e2c81edfd849f7077
delee3064 <delee3064@gmail.com>
2023-01-13 12:57:55 +0900

    fix : setting mysql

--- src/main/java/com/example/myreform/TestController.java
+++ src/main/java/com/example/myreform/TestController.java
@@ -3,15 +3,14 @@ package com.example.myreform;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 
 @RestController
 public class TestController {
 
     @GetMapping("/test")
-    public String test() throws Exception {
-
-        return "MongoDB Test";
+    public String test(){
+        return "Hello world";
     }
 
 }
+
