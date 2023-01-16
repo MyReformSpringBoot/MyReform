600c1c0c2b98230ec99d9745d4a374abc68cb55f
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-01-16 16:40:06 +0900

    test(User): user 정보 확인을 통한 데이터베이스 연동 확인

---
+++ src/main/java/com/example/myreform/repository/UserRepository.java
@@ -0,0 +1,2 @@
+package com.example.myreform.repository;public class UserRepository {
+}
---
+++ src/test/java/com/example/myreform/repository/UserRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.myreform.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class UserRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    EntityManager em;
+
+    @AfterEach
+    private void after() {
+        em.clear();
+    }
+
+    @Test
+    void existsByNickname_있음() throws Exception {
+        String nickname = "kong";
+        System.out.println(userRepository.existsUserByNickname(nickname));
+
+        nickname = "kung";
+        System.out.println(userRepository.existsUserByNickname(nickname));
+    }
+
+}
