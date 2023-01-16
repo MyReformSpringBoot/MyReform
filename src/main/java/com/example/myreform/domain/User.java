600f5832d263cb8d006b4f9d214f3e0766ceec8a
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-01-16 15:53:18 +0900

    feat(User): 유저 정보 클래스 생성

--- src/main/java/com/example/myreform/domain/User.java
+++ src/main/java/com/example/myreform/domain/User.java
@@ -1,2 +1,28 @@
-package com.example.myreform.domain;public class User {
+package com.example.myreform.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USER")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long user_id;
+
+    @Column(nullable = false) // 회원가입 파트에서 멤버관련 정보 설정
+    private String email;
+    private String id;
+    private String pw;
+    private String nickname;
+    private String intruduction;
+    private String marketing;
+
 }
