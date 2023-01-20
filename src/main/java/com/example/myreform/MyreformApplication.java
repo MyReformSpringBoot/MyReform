7227a429e5b70bc8d0acd9646f81ef55bc71b299
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-01-20 15:05:53 +0900

    fix(error)
    - SignupDTO 빈 설정 에러 해결: SignDTO 클래스 기본 생성자 추가
    - create_at 자동 업데이트 안되는 문제 해결: allication에 EnableJpaAuaditing 추가
    - 컬럼명 인식 불가 문제 해결: createAt -> create_at, updateAt -> update_at
    - 컬럼명 오타 수정: state -> status

--- src/main/java/com/example/myreform/MyreformApplication.java
+++ src/main/java/com/example/myreform/MyreformApplication.java
@@ -2,9 +2,11 @@ package com.example.myreform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing // Time 관련 엔티티 부분 사용을 위해서
 @SpringBootApplication
+@EnableJpaAuditing
 public class MyreformApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/myreform/User/domain/User.java
+++ src/main/java/com/example/myreform/User/domain/User.java
@@ -38,13 +38,15 @@ public class User {
     private boolean marketing;
 
     @CreatedDate
+    @Column(name = "create_at")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(name = "update_at")
     private LocalDateTime updatedAt;
 
     @ColumnDefault("1")
-    private int state;
+    private int status;
 
 
     public enum Role {
