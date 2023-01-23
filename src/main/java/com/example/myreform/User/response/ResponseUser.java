b1b792defc90ee201e0263c9f1bdd9f4081636d5
hil <hil679922@gmail.com>
2023-01-23 22:07:17 +0900

    Adding result with token

--- src/main/java/com/example/myreform/User/response/ResponseUser.java
+++ src/main/java/com/example/myreform/User/response/ResponseUser.java
@@ -1,27 +1,29 @@
 package com.example.myreform.User.response;
 
 import com.example.myreform.validation.ExceptionCode;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
+@Getter
 
 public class ResponseUser {
     private final int status;
     private final String code;
     private final String message;
-
+    @Nullable
+    @Autowired
+    private String token;
     public ResponseUser(ExceptionCode exceptionCode) {
         this.status = exceptionCode.getStatus().getValue();
         this.code = exceptionCode.getCode();
         this.message = exceptionCode.getMessage();
     }
 
-    public int getStatus() {
-        return status;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
+    public ResponseUser(ExceptionCode exceptionCode, @Nullable String token) {
+        this.status = exceptionCode.getStatus().getValue();
+        this.code = exceptionCode.getCode();
+        this.message = exceptionCode.getMessage();
+        this.token = token;
     }
 }
