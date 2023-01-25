d35b8594aec308a82c79073a7ad9ebb025ac36a7
hil <hil679922@gmail.com>
2023-01-26 01:01:32 +0900

    Adding findallbyboardId method

--- src/main/java/com/example/myreform/Board/repository/BoardCategoryRepository.java
+++ src/main/java/com/example/myreform/Board/repository/BoardCategoryRepository.java
@@ -4,6 +4,9 @@ import com.example.myreform.Board.domain.BoardCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BoardCategoryRepository extends JpaRepository<BoardCategory, Long> {
+    List<BoardCategory> findAllByBoardId(Long boardId);
 }
