d0329572be06a5556496072afa4dfc5cdccd4405
hil <hil679922@gmail.com>
2023-01-26 23:22:22 +0900

    Refactor: 카테고리로만 조회

--- src/main/java/com/example/myreform/Board/repository/BoardRepository.java
+++ src/main/java/com/example/myreform/Board/repository/BoardRepository.java
@@ -3,8 +3,11 @@ package com.example.myreform.Board.repository;
 import com.example.myreform.Board.domain.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
@@ -13,5 +16,8 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     Page<Board> findAllByBoardIdLessThanAndStatusEqualsOrderByBoardIdDesc(Long lastBoardId, int status, PageRequest pageRequest);
     Page<Board> findAllByBoardIdLessThanAndStatusEqualsAndTitleContainingOrderByBoardIdDesc(Long lastBoardId, int status, String title, PageRequest pageRequest);
+    Page<Board> findAllByBoardIdLessThanAndBoardIdInOrderByBoardIdDesc(Long lastBoardId, List<Long> boards, PageRequest pageRequest);
     Board findBoardByBoardId(Long boardId);
+
+    //Page<Board> findAllByBoardIdInOrderByBoardIdDesc(List<Long> boards, PageRequest pageRequest);
 }
